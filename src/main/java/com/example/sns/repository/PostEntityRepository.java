c110d23fb20ac90f0d625e006f497b59480842a8
nooose <hd15807@gmail.com>
2022-08-28 01:41:20 +0900

    feat: 피드목록 불러오기 기능 구현

--- src/main/java/com/example/sns/repository/PostEntityRepository.java
+++ src/main/java/com/example/sns/repository/PostEntityRepository.java
@@ -1,9 +1,13 @@
 package com.example.sns.repository;
 
 import com.example.sns.model.Entity.PostEntity;
+import com.example.sns.model.Entity.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostEntityRepository extends JpaRepository<PostEntity, Integer> {
+    Page<PostEntity> findAllByUser(UserEntity entity, Pageable pageable);
 }
